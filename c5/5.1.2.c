e135a9c040c87ff74285e6cde0db4ebfe858c24f
fancivez <fancywu27@163.com>
2017-04-05 13:48:02 +0800

    5.1.2通用socket地址

--- c5/5.1.2.c
+++ c5/5.1.2.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <sys/socket.h>
 
-struct sockaddr {
+//旧通用socket
+struct sockaddr_fancivez {
     sa_family_t sa_family;
     char sa_data[14];
 };
+
+//不声明会报 ‘__ss_align’ undeclared here (not in a function)
+unsigned long int __ss_align;
+
+//新通用socket,内存对齐
+struct sockaddr_storage_fancivez {
+    sa_family_t sa_family;
+    unsigned long int __ss_align;
+    char __ss_padding[128-sizeof(__ss_align)];
+};
+
+int main(int argc, char const *argv[]) {
+    printf("hello world！");
+    return 0;
+}
