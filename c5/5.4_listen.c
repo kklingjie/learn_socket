ef631b37d760c01bcaf70048744e557b61e4566e
fancivez <fancywu27@163.com>
2017-04-11 14:07:40 +0800

    listen补全

--- c5/5.4_listen.c
+++ c5/5.4_listen.c
@@ -28,21 +28,37 @@ int main(int argc, char const *argv[]) {
     signal(SIGTERM,handle_term);
 
     if (argc <= 3) {
-        printf("useage: %s ip_address port_number backlog\n", basename[0]);
+        printf("useage: %s ip_address port_number backlog\n", basename(argv[0]));
         return 1;
     }
 
     const char* ip = argv[1];
-
+    //atoi:ascii to integer
+    // "1234" => 1234
     int port = atoi(argv[2]);
     int backlgo = atoi(argv[3]);
 
     int sock = socket(PF_INET, SOCK_STRAM, 0);
+    //如果为false, stderr打印错误信息, abort终止程序
     assert(sock >= 0 );
 
     struct sockaddr_in address;
+    //推荐使用memset memset(&address, 0, sizeof(address));
     bzero(&address, sizeof(address));
     address.sin_family = AF_INET;
+    //点分十 > 二进制
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    //调整字节序 host to network sequence
+    address.sin_port = htons(port);
+
+    int ret = bind(sock, (struct sockaddr*)&address, sizeof(address));
+    assert(ret != -1);
+
+    while (!stop) {
+        sleep(1);
+    }
+
+    close(sock);
 
     return 0;
 }
